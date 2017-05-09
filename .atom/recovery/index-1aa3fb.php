50259e373722b6dbe8f132def44989ef9ed505e6
aewens <email@aewens.com>
2017-05-09 11:13:41 -0500

    Perfecting the system

---
+++ .atom/recovery/index-1aa3fb.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Austin Ewens, aewens, @aewens_">
+        <meta name="description" content="My portfolio site">
+        <title>Austin Ewens</title>
+        <link rel="stylesheet" href="public/stylesheets/style.css">
+    </head>
+    <body>
+        <div id="app">
+            <header>
+                <img src="public/images/logo.png" alt="logo">
+                <div id="name">
+                    <h1>austin</h1>
+                    <h1>ewens</h1>
+                </div>
+            </header>
+            <section>
+                <article>
+                    <div class="blurb">
+                        Places you can find me:
+                        <br>
+                        <a href="https://github.com/aewens">Github</a>, 
+                        <a href="https://twitter.com/ewensian">Twitter</a>, 
+                        and my <a href="https://blog.aewens.com">Blog</a>.
+                    </div>
+                    <img src="public/images/about.png" alt="about">
+                </article>
+                <article>
+                    <div class="blurb">
+                        I am a web developer and designer that dabbles in tech 
+                        support and minimalistic graphics design.
+                    </div>
+                    <img src="public/images/skills.png" alt="skills">
+                </article>
+                <article>
+                    <div class="blurb">
+                        <span>
+                            <a href="mailto:email@aewens.com">
+                                email@aewens.com
+                            </a>
+                        </span>
+                    </div>
+                    <img src="public/images/contact.png" alt="contact">
+                </article>
+            </section>
+        </div>
+    </body>
+</html>
